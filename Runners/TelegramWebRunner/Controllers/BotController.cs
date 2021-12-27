60b60e547b8338d3c88df5a3b93f7f17961f5c4f
shabi mak <shabimak@gmail.com>
2021-12-27 22:18:58 +0200

    Add telegram runner

---
+++ Runners/TelegramWebRunner/Controllers/BotController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramWebRunner.Controllers
+{
+    [ApiController]
+    [Route("api/bot")]
+    public class BotController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            var value = Environment.GetEnvironmentVariable("TelegramKey");
+            var client = new TelegramBotClient(value);
+            
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+
+            return Ok();
+        }
+    }
+}
