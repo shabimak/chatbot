61dc3e154a705a9057ac36e64fff75962058cace
shabi mak <shabimak@gmail.com>
2021-12-27 22:41:22 +0200

    Connect to main infrastructure

--- Runners/TelegramWebRunner/Controllers/BotController.cs
+++ Runners/TelegramWebRunner/Controllers/BotController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dals;
+using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace TelegramWebRunner.Controllers
 {
@@ -14,21 +17,19 @@ namespace TelegramWebRunner.Controllers
     public class BotController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            return Ok();
-        }
-
+        public IActionResult Get() => Content("Hi");
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Update update)
         {
             var value = Environment.GetEnvironmentVariable("TelegramKey");
             var client = new TelegramBotClient(value);
-            
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            var host = new Host(new MemoryDal(), new PluginsMenu(), new PluginsManager());
+
+            if (update.Type == UpdateType.Message)
             {
-                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+                var result = host.Run(update.Message.Text, update.Message.From.Id.ToString());
+                await client.SendTextMessageAsync(update.Message.From.Id, result);
             }
 
             return Ok();
